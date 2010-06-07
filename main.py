9c768cfc754511eaa63b881cceca40c2824f68d9
Joe Shaw <joe@joeshaw.org>
2010-06-06 23:31:08 -0400

    largely refactor/rewrite the JS code
    1. Use ProximoBus as the API provider rather than NextBus
    
       This lets us use JSONP so that we don't need GAE to proxy requests
       for us anymore.  The index.html file could now be served from any
       host without modifications to the server necessary.  ProximoBus is
       just a transformation of the NextBus API, so it's handy.
    
    2. Use the Google Maps v3 API, where we were using v2 before.  This
       makes mobile devices happier and ends up making the structure of
       the code a bit better in places too.
    
    3. De-uglify things from back when I barely knew JS.

--- main.py
+++ main.py
@@ -11,29 +11,8 @@ class MainHandler(RequestHandler):
 	def get(self):
 		self.response.out.write(template.render('index.html', {}))
 
-class GetRoutesHandler(RequestHandler):
-	def get(self):
-		self.response.headers['Content-Type'] = 'text/xml'
-		r = fetch('http://webservices.nextbus.com/service/publicXMLFeed?command=routeList&a=mbta')
-		self.response.out.write(r.content)
-		
-class VehicleLocationsHandler(RequestHandler):
-	last_request_time = {}
-	
-	def get(self):
-		route = int(self.request.get('route'))
-
-		last_time = self.last_request_time.get(route, 0)
-		
-		self.response.headers['Content-Type'] = 'text/xml'
-		r = fetch('http://webservices.nextbus.com/service/publicXMLFeed?command=vehicleLocations&a=mbta&r=%d&t=%d'
-		          % (route, last_time))
-		self.response.out.write(r.content)
-
 handlers = [
 	('/', MainHandler),
-	('/getroutes/', GetRoutesHandler),
-	('/vehiclelocations/', VehicleLocationsHandler)
 ]
 
 def main():
